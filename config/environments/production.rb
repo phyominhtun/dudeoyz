7c3b063f10a3b1346ebce6ea2329a9bd5044ad55
phyo min htun <blackrevolution.x@gmail.cm>
2016-07-19 02:37:43 +0630

    Testing V 1.4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'oyz.herokuapp.com' }
-  config.mailer_sender = 'oyz.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'oyz.heroku.com' }
 end
