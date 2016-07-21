50662c20a37f1a283ef6f9e105494d1e77c8acb9
phyo min htun <blackrevolution.x@gmail.cm>
2016-07-21 19:08:06 +0630

    removed name and website from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627024329) do
+ActiveRecord::Schema.define(version: 20160528135538) do
 
   create_table "ahoy_events", force: :cascade do |t|
     t.integer  "visit_id"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20160627024329) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.string   "title"
   end
 
   create_table "users", force: :cascade do |t|
