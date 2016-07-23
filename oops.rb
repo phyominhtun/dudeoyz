f85abb3532de1b4d1389b5224f8f83c9e346bf7c
phyo min htun <blackrevolution.x@gmail.cm>
2016-07-23 21:45:24 +0630

    well

--- oops.rb
+++ oops.rb
@@ -1,7 +1,7 @@
 require "rest-client"
-
-RestClient.proxy = ENV["QUOTAGUARDSTATIC_URL"]
-
-res = RestClient.get("http://52.86.18.14.quotaguard.com")
-
-puts "Your Static IP is: 52.86.18.14"
+ 
+RestClient.proxy = ENV["QUOTAGUARDSTATIC_URL"] if ENV["QUOTAGUARDSTATIC_URL"]
+ 
+res = RestClient.get("http://quotaguard6220:b9c1d6580cc0@us-east-static-01.quotaguard.com:9293")
+ 
+puts "Your IP was: #{res.body}"
