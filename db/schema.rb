e71323fc1e77f6109e10ef94db6c1eb37b6e7e54
phyo min htun <blackrevolution.x@gmail.cm>
2016-07-23 15:45:37 +0630

    still buggy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528135538) do
+ActiveRecord::Schema.define(version: 20160627024329) do
 
   create_table "ahoy_events", force: :cascade do |t|
     t.integer  "visit_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160528135538) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "title"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,8 +47,6 @@ ActiveRecord::Schema.define(version: 20160528135538) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
-    t.string   "website"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
